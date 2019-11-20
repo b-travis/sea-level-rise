ce077b8774535afe44f52685c047317ca6856ffa
Barrett Travis <51933861+b-travis@users.noreply.github.com>
2019-11-19 20:23:53 -0800

    Update miami_SLR_modeling.R - complete "tide" section
    Added little plot of tide over a month. Looks good!

--- miami_SLR_modeling.R
+++ miami_SLR_modeling.R
@@ -1,5 +1,12 @@
 #### FLOODING, MIAMI FL ------------------------------------------------
 
+
+#### Set-up & load packages --------------------------------------------
+library(tidyverse)
+
+# Create a theme for plotting
+my_theme <- theme_bw()
+
 #### Create Initial Function -------------------------------------------
 N_YEARS <- 80
 N_HOUR_IN_YEAR <- 24 * 365 - 0.25*1
@@ -7,17 +14,27 @@ t = (0:(N_HOUR_IN_YEAR * N_YEARS)) # [hours]
 
 # tide:
 LUNAR_TIDAL_PERIOD <- 12.42 # [hours] source: wikipedia
-LUNAR_AVG_TIDE_MAGNITUDE <- 0.5 # [m]
+LUNAR_AVG_TIDE_MAGNITUDE <- 0.33 # [m]
 lunar_tide = LUNAR_AVG_TIDE_MAGNITUDE * sin((2*pi/LUNAR_TIDAL_PERIOD) * t)
 
 SOLAR_TIDAL_PERIOD <- 12 # [hours]
 SOLAR_AVG_TIDE_MAGNITUDE <- 0.46 * LUNAR_AVG_TIDE_MAGNITUDE # [m] source: https://oceanservice.noaa.gov/education/tutorial_tides/media/supp_tide02.html
 solar_tide = SOLAR_AVG_TIDE_MAGNITUDE * sin((2*pi/SOLAR_TIDAL_PERIOD) * t - 3) # staggered from the lunar tide
 
+# Other possible adds:
 # perigee = closest point of moon to earth
+# closest point of earth to sun
+# whatever contributes to the late summer/fall king tides in South Florida
+
+tide <- data.frame(t = t, tide = lunar_tide + solar_tide) %>%
+  mutate(t_days = t/24)
+
+# Plot the simple tide
+ggplot(tide %>% filter(t_days < 32), aes(x = t_days, y = tide)) + 
+  geom_line() +
+  my_theme
 
 
-tide <- lunar_tide + solar_tide
 
 # Add flood event forcing --------------------------------------------------
 set.seed(54) # set seed to 54 (random)
